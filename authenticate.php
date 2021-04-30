d9a95caa6d481320872263e7a034419e5c33cf46
kesiyaraj <kesiya.raj@sjsu.edu>
2021-04-30 13:28:06 -0700

    Front End Design

--- authenticate.php
+++ authenticate.php
@@ -39,23 +39,61 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html lang="en">
     <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
     </head>
     <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Shipping Port</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>
         <div>
-            <header>
-                <a href="index.php">Home</a>
-                <h1>Login</h1>        
-            </header>
             <section>
                 <form id="login" method="post">
-                    <label for="user_id">User_ID:</label>
+
+
+                <div class="row">
+                    <div class="col-4 offset-4">
+                        <div class="mb-3">
+                            <label for="user_id" class="form-label">User Id</label>
+                            <input id="user_id" name="user_id" type="number" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input class="form-control" id="email" name="email" type="text"  aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input  class="form-control" id="password" name="password" type="password" >
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block" style="width:100%">Login </button>
+                        
+                    </div>
+                </div>
+
+                    <!-- <label for="user_id">User_ID:</label>
                     <input id="user_id" name="user_id" type="number" required>
                     <label for="email">Email:</label>
                     <input id="email" name="email" type="text" required>
                     <label for="password">Password:</label>
                     <input id="password" name="password" type="password" required>                    
                     <br />
-                    <input type="submit" value="Login">
+                    <input type="submit" value="Login"> -->
                 </form>
             </section>
         </div>
